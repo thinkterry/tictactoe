21147e4c61a63e38ce733c30c0dc1aaaef5a2cbd
Terry Smith <terry@kulacauses.com>
2013-08-04 15:52:54 -0600

    Stopped populating board when a winner exists.

--- tictactoe.py
+++ tictactoe.py
@@ -44,19 +44,39 @@ class Board():
         self.board[i_rand][j_rand].val(player)
 
     def full(self):
-        """ Innocent unless proven guilty """
+        """ Innocent until proven guilty """
 
         for i in range(self.SIZE):
             for j in range(self.SIZE):
                 if self.board[i][j].val() == Cell.EMPTY:
                     return False
-        
         return True
+
+    def done(self):
+        return self.winner() or self.full()
+
+    def winner(self):
+        horizontal = (
+            (self.board[0][0].val() == self.board[0][1].val() == self.board[0][2].val() != Cell.EMPTY) or
+            (self.board[1][0].val() == self.board[1][1].val() == self.board[1][2].val() != Cell.EMPTY) or
+            (self.board[2][0].val() == self.board[2][1].val() == self.board[2][2].val() != Cell.EMPTY)
+        )
+        vertical = (
+            (self.board[0][0].val() == self.board[1][0].val() == self.board[2][0].val() != Cell.EMPTY) or
+            (self.board[0][1].val() == self.board[1][1].val() == self.board[2][1].val() != Cell.EMPTY) or
+            (self.board[0][2].val() == self.board[1][2].val() == self.board[2][2].val() != Cell.EMPTY)
+        )
+        diagonal = (
+            (self.board[0][0].val() == self.board[1][1].val() == self.board[2][2].val() != Cell.EMPTY) or
+            (self.board[0][2].val() == self.board[1][1].val() == self.board[2][0].val() != Cell.EMPTY)
+        )
+
+        return horizontal or vertical or diagonal
 
 if __name__ == '__main__':
     board = Board()
 
-    while not board.full():
+    while not board.done():
         board.move(Cell.X)
         board.move(Cell.O)
 
