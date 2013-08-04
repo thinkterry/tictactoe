5f19a6d8be26475ac175c0e21b2160b1dad01f79
Terry Smith <terry@kulacauses.com>
2013-08-04 15:36:02 -0600

    Populated board until it's full.

--- tictactoe.py
+++ tictactoe.py
@@ -1,9 +1,21 @@
 # Tic-tac-toe
 
+import random
+
 class Cell():
 
+    EMPTY = ' '
+    X = 'X'
+    O = 'O'
+
     def __init__(self):
-        self.val = ' '
+        self._val = Cell.EMPTY
+
+    def val(self, new_val=None):
+        if new_val is None:  # get
+            return self._val
+        else:  # set
+            self._val = new_val
 
 class Board():
 
@@ -20,11 +32,32 @@ class Board():
         return retval
 
     def repr(self):
+        print
         for i in range(self.SIZE):
             print ('+ - ' * self.SIZE) + '+'
-            print '| ' + ' | '.join([self.board[i][j].val for j in range(self.SIZE)]) + ' |'
+            print '| ' + ' | '.join([self.board[i][j].val() for j in range(self.SIZE)]) + ' |'
         print '+ - ' * self.SIZE + '+'
 
+    def move(self, player):
+        i_rand = random.choice(range(3))
+        j_rand = random.choice(range(3))
+        self.board[i_rand][j_rand].val(player)
+
+    def full(self):
+        """ Innocent unless proven guilty """
+
+        for i in range(self.SIZE):
+            for j in range(self.SIZE):
+                if self.board[i][j].val() == Cell.EMPTY:
+                    return False
+        
+        return True
+
 if __name__ == '__main__':
-    board = Board() 
+    board = Board()
+
+    while not board.full():
+        board.move(Cell.X)
+        board.move(Cell.O)
+
     board.repr()
